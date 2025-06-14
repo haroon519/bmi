8ae4b621f8ca9937002454641914139934c3c758
haroon519 <haroon.alrashidy.278@gmail.com>
2025-06-14 10:41:26 +0300

    Add files via upload

--- first.py
+++ first.py
@@ -1,8 +1,8 @@
 import streamlit as st
 weight = st.number_input(
     label='enter wight(kg):',
-    min_value=5
-    max_value=500
+    min_value=5,
+    max_value=500,
     value=50
 
 )
@@ -12,12 +12,12 @@ weight = st.number_input(
 
 hight = st.number_input(
     label='enter wight(kg):',
-    min_value=5
-    max_value=500
-    value=50
+    min_value=5,
+    max_value=500,
+    value=50,
     step=0.01
     
 )
 if st.button('calculater'):
     bmi = weight/hight **2
-    st.write(f'your bmii is: '{bmi})
+    st.write(f'your bmii is: {bmi}')
